d1df01d1f35e5b29748c42ab8bf9c8bc486741a1
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-02 03:07:59 +0500

    Task4

---
+++ EXAMPLE04/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Write("Введите число:");
+ int num = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+even = even + 2;
+
+}
+
