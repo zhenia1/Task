c7e0a2549907d4a15faa405c5fdbcc9f74ded121
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-03 17:15:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите чило:");
+int num = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num % 2 == 1)
+ {
+    Console.WriteLine("Число" + num + "явлается: Нечётным");
+}
+else
+ {
+    Console.WriteLine("Число" + num + "явлется: Чётным");
+}
